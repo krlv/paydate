8c3c3f98ec20071047af58cdc550d181732f4147
krlv <eug.krlv@gmail.com>
2019-04-03 19:37:41 -0700

    Use visibility trait to test Calculator's private properties

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -6,12 +6,20 @@ namespace DevXyz\Challenge\Paydate\Test;
 
 use DevXyz\Challenge\Paydate\Calculator;
 use DevXyz\Challenge\Paydate\CalculatorInterface;
+use DevXyz\Challenge\Paydate\Test\Traits\VisibilityTrait;
 use PHPUnit\Framework\TestCase;
 
 final class CalculatorTest extends TestCase
 {
+    use VisibilityTrait;
+
     public function testCalculatePaydates(): void
     {
-        new Calculator(CalculatorInterface::PAYDATE_MODEL_WEEKLY);
+        $calculator = new Calculator($model = CalculatorInterface::PAYDATE_MODEL_WEEKLY);
+        $this->assertEquals($model, $this->getPrivateProperty($calculator, 'model'));
+        $this->assertEmpty($this->getPrivateProperty($calculator, 'holidays'));
+
+        $calculator = new Calculator($model = CalculatorInterface::PAYDATE_MODEL_WEEKLY, $holidays = ['2019-01-01']);
+        $this->assertEquals($holidays, $this->getPrivateProperty($calculator, 'holidays'));
     }
 }
---
+++ tests/Traits/VisibilityTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevXyz\Challenge\Paydate\Test\Traits;
+
+trait VisibilityTrait
+{
+    /**
+     * Set private property to an object.
+     *
+     * @param object $object
+     * @param string $property
+     * @param mixed  $value
+     */
+    protected function setPrivateProperty(object $object, string $property, $value): void
+    {
+        \Closure::bind(static function ($object, $value) use ($property): void {
+            $object->$property = $value;
+        }, null, $object)->__invoke($object, $value);
+    }
+
+    /**
+     * Get private property from an object.
+     *
+     * @param object $object
+     * @param string $property
+     *
+     * @return mixed
+     */
+    protected function getPrivateProperty(object $object, string $property)
+    {
+        return \Closure::bind(static function ($object) use ($property) {
+            return $object->$property;
+        }, null, $object)->__invoke($object);
+    }
+}
